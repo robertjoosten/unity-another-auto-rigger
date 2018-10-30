e812d7b98347746dd59bae7f31fc9db1a224df7c
Robert Joosten <rwm.joosten@gmail.com>
2018-10-30 21:04:58 +0000

    rename helper transform to translate

--- Assets/Scripts/RuntimeJoints/RuntimeHelperTranslate.cs
+++ Assets/Scripts/RuntimeJoints/RuntimeHelperTranslate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class RuntimeHelperTransform : MonoBehaviour {
+public class RuntimeHelperTranslate : MonoBehaviour {
 
     [Header("[Pose Reader]")]
     [Space(5)]
